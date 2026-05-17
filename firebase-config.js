bcd306c46391047f3f3319f01a565d128a5908af
groekens <67225369+groekens@users.noreply.github.com>
2026-05-17 12:43:03 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase configuration for Dooodle
+firebase.initializeApp({
+  apiKey: "AIzaSyBFubNBtvGXJDG7TubBJ-REiaqaTSUFMX0",
+  authDomain: "dooodle-363fd.firebaseapp.com",
+  projectId: "dooodle-363fd",
+  storageBucket: "dooodle-363fd.firebasestorage.app",
+  messagingSenderId: "325060168942",
+  appId: "1:325060168942:web:5731f4ae9bdd975eb177ff"
+});
+const db = firebase.firestore();
